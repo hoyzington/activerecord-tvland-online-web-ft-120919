7af1b6cf74cffa5ee82a9ee46a566cc1112c23a8
Scott Hoisington <hoyzington@gmail.com>
2020-01-29 05:47:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
   
   def actors_list
     self.characters.map {|c| "#{c.actor.full_name}"}
