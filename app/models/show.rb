1de15cfb1a6df2ab267048c93efcfa25d1101963
Scott Hoisington <hoyzington@gmail.com>
2020-01-29 05:23:40 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list
-    
+    actors = self.characters.actors
+    actors.map {|a| "#{a.full_name}"}
   end 
   
 end
